965ed037f0012ecb12af71205fc64355cadd017d
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 17:14:56 +0300

    Rokas

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDyuLPJ2T_dXJMMRzXOpDQHv5p4rF73e3E",
+    authDomain: "brainlyemails-app.firebaseapp.com",
+    projectId: "brainlyemails-app",
+    storageBucket: "brainlyemails-app.appspot.com",
+    messagingSenderId: "569505406293",
+    appId: "1:569505406293:web:1b4ab8f4a45ced191723dc",
+    measurementId: "G-TCSL5LRQFF"
+};
+
